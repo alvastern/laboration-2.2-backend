e5b82941d2ec0d6cbb4f85f2d7226d2143fdcb81
alvastern <alva.stern02@gmail.com>
2026-04-17 14:25:26 +0200

    Slutgiltig funktionalitet

--- src/js/add.js
+++ src/js/add.js
@@ -2,10 +2,11 @@
 
 import "../css/main.scss";
 
-const APIURL = "http://localhost:3000/api/workexperience";
-const form = document.getElementById("form");
-const messageEl = document.getElementById("message");
+const APIURL = "http://localhost:3000/api/workexperience";  // Variabel som lagrar länken till APIet
+const form = document.getElementById("form");  // Hämtar formuläret som användaren fyller i för att lägga till en post från DOM
+const messageEl = document.getElementById("message");  // Hämtar felmeddelande från DOM
 
+// Funktion för att validera så att alla fält i formuläret är ifyllda innan posten läggs till
 function validateForm(data) {
     const errors = []
 
@@ -19,9 +20,11 @@ function validateForm(data) {
     return errors;
 }
 
+// Eventlyssnare som lyssnar på när formuläret skickas in
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
+    // Läser det som användaren har skrivit i formuläret
     const formData = {
         company_name: form.company_name.value,
         position: form.position.value,
@@ -33,11 +36,13 @@ form.addEventListener("submit", async (e) => {
 
     const errorsForm = validateForm(formData);
 
+    // Skapar felmeddelande om något inte är ifyllt
     if (errorsForm.length > 0) {
         messageEl.innerHTML = errorsForm.map(err => `<p>${err}</p>`).join("");
         return;
     }
 
+    // Fetchar APIet med metoden post och skapar då innehåll till en ny punkt
     try {
         const response = await fetch(APIURL, {
             method: "post",
@@ -47,6 +52,7 @@ form.addEventListener("submit", async (e) => {
             body: JSON.stringify(formData)
         });
 
+        // Går innehållet inte att lagra skrivs det ut ett felmeddelande
         if (!response.ok) {
             throw new Error("Fel vid lagring");
         }
