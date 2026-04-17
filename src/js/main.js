bed7cb22c5a510d5c4035db0ec8c48f12c709b24
alvastern <alva.stern02@gmail.com>
2026-04-17 14:25:26 +0200

    Slutgiltig funktionalitet

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,10 @@
 
 import "../css/main.scss";
 
-const APIURL = "http://localhost:3000/api/workexperience";
-const experienceList = document.getElementById("erfarenhet-lista");
+const APIURL = "http://localhost:3000/api/workexperience";  // Variabel som lagrar länken till APIet
+const experienceList = document.getElementById("erfarenhet-lista");  // Hämtar listan där erfarenheterna lagras från DOM
 
+// Funktion för att anropa APIet med fetch, async, await, try och catch
 async function loadWorkExperience () {
     try {
         const response = await fetch(APIURL);
@@ -12,10 +13,12 @@ async function loadWorkExperience () {
 
         experienceList.innerHTML = "";
 
+        // Loopar igenom listan för att visa punkterna
         data.forEach(item => {
             const li = document.createElement("li");
             li.classList.add("experience-item");
 
+            // Skapar text för de olika delarna i varje punkt i listan
             li.innerHTML = `
             <h3>${item.company_name}</h3>
             <p>${item.position}</p>
@@ -28,14 +31,16 @@ async function loadWorkExperience () {
             experienceList.appendChild(li);
         });
     } catch (error) {
-        experienceList.innerHTML = "<p>Kunde inte läsa in data</p>"
+        experienceList.innerHTML = "<p>Kunde inte läsa in data</p>"  // Felmeddleande om datan inte kan läsas in
     }
 }
 
+// Eventlyssnare för delete-knappen som tar bort en post
 document.addEventListener("click", async (e) => {
     if (e.target.matches("button[data-id]")) {
         const id = e.target.dataset.id;
 
+        // Använder fetch och metoden DELETE för att radera en punkt
         try {
             await fetch(`${APIURL}/${id}`, {
                 method: "DELETE"
